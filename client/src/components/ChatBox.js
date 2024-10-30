6e2e0f741f2ca48732d04806695cb9271c98d3a6
Ansh Kakkar <141085661+anshk8@users.noreply.github.com>
2024-10-30 17:44:17 -0400

    Update ChatBox.js

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -23,7 +23,7 @@ const ChatBox = () => {
     try {
 
       //Response from backend
-        const res = await axios.post('http://localhost:5001/api/message', { message });
+        const res = await axios.post('https://resume-chat-bot-mmlx-client-git-server-anshs-projects-01f89063.vercel.app/api/message', { message });
 
         //Debuging log
         console.log("Received response:", res.data.result);  
